22e13c16d9b536c8b9c48fd3ad9de7d87e8b6296
Onil Pravasi <onilpravasi@yahoo.com>
2019-05-22 16:13:04 -0500

    added files to github

---
+++ myretrofitlibrary/src/main/java/com/example/myretrofitlibrary/MyRetroInstance.java
@@ -0,0 +1,18 @@
+package com.example.myretrofitlibrary;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MyRetroInstance {
+
+    public static Retrofit retrofit;
+
+    public Retrofit getMyRetrofitInstance(String baseUrl){
+
+        retrofit = new retrofit2.Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
